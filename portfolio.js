eb8f018eb84ed6da1914a974f72af28480040952
mcgowm <75761433+mcgowm@users.noreply.github.com>
2023-05-26 21:32:29 -0700

    carl

--- portfolio.js
+++ portfolio.js
@@ -1,2 +1,66 @@
-"use_strict"
-    (function () { })();
+"use strict";
+
+(function () {
+
+    window.addEventListener("load", init);
+
+    /**
+     * Applies event listeners to core buttons and starts process of fetching all pokemon from server
+     */
+    function init() {
+
+    }
+
+
+
+    /** ------------------------------ Helper Functions  ------------------------------ */
+
+    /**
+     * Returns the element that has the ID attribute with the specified value.
+     * @param {string} idName element ID
+     * @returns {object} DOM object associated with id.
+     */
+    function id(idName) {
+        return document.getElementById(idName);
+    }
+
+    /**
+     * Returns the first element that matches the given CSS selector.
+     * @param {string} selector CSS query selector.
+     * @returns {object} The first DOM object matching the query.
+     */
+    function qs(selector) {
+        return document.querySelector(selector);
+    }
+
+    /**
+     * Returns the array of elements that match the given CSS selector.
+     * @param {string} selector CSS query selector
+     * @returns {object[]} array of DOM objects matching the query.
+     */
+    function qsa(selector) {
+        return document.querySelectorAll(selector);
+    }
+
+    /**
+     * Creates a dom element given a proper name
+     * @param {string} tagName a string which is use to create the element
+     * @returns {object} generated Dom element
+     */
+    function gen(tagName) {
+        return document.createElement(tagName);
+    }
+
+    /**
+     * This is a CSE 154 provided function. It checks if a fetch request has
+     * successfully gone through
+     * @param {fetchresult} res A result from the fetch function
+     * @returns {fetchresult} a guaranteed successful result
+     */
+    async function checkStatus(res) {
+        if (!res.ok) {
+            throw new Error(await res.text());
+        }
+        return res;
+    }
+})();
